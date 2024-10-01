9677afa119001d53b79aed91f83eabe6ef96b605
nova1006 <vitali.mack@gmx.de>
2024-10-01 21:11:28 +0200

    file2 create

---
+++ aufgabe-02.py
@@ -0,0 +1,12 @@
+# Variablen Definition
+a = 10 # Ganzzahl
+b = 3 # Ganzzahl
+
+# Berechnungen
+print("Wir haben zwei Zahlen: 10 und 3. Damit fuehren wir verschiedene mathematische Operationen durch.")
+print("Summe:", a + b)
+print("Differenz:", a - b)
+print("Produkt:", a * b)
+print("Ganzzahliger Quotierung:", a // b)
+print("Rest der Division:", a % b)
+print("a hoch b:", a ** b)
